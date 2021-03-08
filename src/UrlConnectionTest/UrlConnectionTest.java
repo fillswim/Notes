10ae51ef6930780ba7f3abf6aa5ea723f493781a
Oleg Filatov <fillswim@gmail.com>
2021-03-08 13:59:36 +0300

    Add UrlConnectionTest

---
+++ src/UrlConnectionTest/UrlConnectionTest.java
@@ -0,0 +1,54 @@
+package UrlConnectionTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlConnectionTest {
+    public static void main(String[] args){
+//        String urlAddress = "http://google.com";
+        String urlAddress = "https://query1.finance.yahoo.com/v7/finance/download/GC=F?period1=1614902400&period2=1615161600&interval=1d&events=history&includeAdjustedClose=true";
+        URLConnection urlConnection;
+        URL url;
+
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            url = new URL(urlAddress);
+            urlConnection = url.openConnection();
+
+            inputStreamReader = new InputStreamReader(urlConnection.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+
+            if (inputStreamReader != null){
+                try {
+                    inputStreamReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (bufferedReader != null){
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
